c254fd06066e251935b230ee2774ad1d435a18fa
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-03-20 23:54:11 -0300

    FEAT(ReajusteService): Refatorando reajustarSalarioDoFuncionario

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,21 +1,21 @@
 package br.com.alura.rh.service;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
-
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
+import java.util.List;
 
 public class ReajusteService {
-    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
-        BigDecimal salarioAtual = funcionario.getSalario();
-        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
 
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+    public List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        this.validacoes.forEach(v -> v.validar(funcionario,aumento));
+
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizarSalario(salarioReajustado);
     }
 }
