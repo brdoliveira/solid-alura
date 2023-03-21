2ed1e7d7a3b25a2007c3d9676184cc4949d6aad4
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-03-20 23:55:19 -0300

    FEAT(ValidacaoPeridiocidadeEntreReajuste): Criando classeValidacaoPeridiocidadeEntreReajuste

---
+++ src/br/com/alura/rh/service/ValidacaoPeridiocidadeEntreReajuste.java
@@ -0,0 +1,20 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ValidacaoPeridiocidadeEntreReajuste implements ValidacaoReajuste{
+
+    public void validar(Funcionario funcionario, BigDecimal aumento){
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDeseUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste,dataAtual);
+        if(mesesDeseUltimoReajuste < 6){
+            throw new ValidacaoException("Intervalo entre reajustes deve ser de no minimo de 6 meses!");
+        }
+
+    }
+}
